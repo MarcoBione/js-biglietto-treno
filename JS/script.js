b94eeb88e68186c7c393135541a8d2c7abb912b6
MarcoBione <marcobione.work@gmail.com>
2023-03-08 16:17:39 +0100

    Add JS functions + calc and discount

--- JS/script.js
+++ JS/script.js
@@ -26,29 +26,69 @@ console.log(UserAge);
 //KmPrice
 let Kmprice = 0.21;
 
+//junior
+let junior = 18;
+
+//senior
+let senior = 65;
+
 //20%discount
 const Discount20 = 20;
+
+//DiscountJunior
+let DiscountJunior = '';
 
 //40%discount
 const Discount40 = 40;
 
+//DiscountSenior
+let DiscountSenior = '';
+
 //price
-let Price ='';
+let Price = '';
 
 //finalprice
-let FinalPrice ='';
+let FinalPrice = '';
 
 
 //check if the user put correct values
-if(!isNaN(UserKm) && !isNaN(UserAge)){
+if (!isNaN(UserKm) && !isNaN(UserAge)) {
 
-    let FinalPrice = UserKm * Kmprice;
-    console.log(FinalPrice);
-    document.getElementById('price').innerHTML = `
-    <p>Il prezzo del suo biglietto:${FinalPrice}&euro;</p>
-    `;
-} else{
-    document.getElementById ('ticket').innerHTML =`
-    <p>I valori da lei inseriti non sono corretti, perfavore ritenti!</p>
+    
+    if (UserAge <= junior) {
+        //For under 18
+        let DiscountJunior = ((UserKm * Kmprice) * Discount20 / 100).toFixed(2);
+        console.log(DiscountJunior);
+        //final calc
+        let FinalPrice= ((UserKm * Kmprice) - DiscountJunior).toFixed(2);
+        document.getElementById('price').innerHTML = `
+        <p>Il prezzo del suo biglietto: ${FinalPrice}&euro;</p>
+        <p>Con uno sconto pari a: ${DiscountJunior}&euro;</p>
+        `;
+
+    }else if (UserAge >= senior){
+        //for senior over 65
+        let DiscountSenior = ((UserKm * Kmprice) * Discount40 / 100).toFixed(2);
+        console.log(DiscountSenior);
+        //final calc
+        let FinalPrice = ((UserKm * Kmprice) - DiscountSenior).toFixed(2);
+        document.getElementById('price').innerHTML = `
+        <p>Il prezzo del suo biglietto:${FinalPrice}&euro;</p>
+        <p>Con uno sconto pari a: ${DiscountSenior}&euro;</p>
+        `;
+
+    }else {
+        //calc if the User is > 18 <65
+        let FinalPrice = (UserKm * Kmprice).toFixed(2);
+        console.log(FinalPrice);
+        document.getElementById('price').innerHTML = `
+        <p>Il prezzo del suo biglietto:${FinalPrice}&euro;</p>
+        <p class="text-danger">Purtroppo non hai diritto ad uno sconto</p>
+        `;
+    }
+
+}else {
+    document.getElementById('ticket').innerHTML = `
+    <p class="text-danger">I valori da lei inseriti non sono corretti, perfavore ritenti!</p>
     `;
 }
